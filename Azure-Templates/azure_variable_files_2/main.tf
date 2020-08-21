73f24db4494e8c24c24160a8ba4a403b84b94d79
Muhammad Rehan <66232567+mxrehan@users.noreply.github.com>
2020-08-21 16:37:39 -0700

    Create main.tf

---
+++ Azure-Templates/azure_variable_files_2/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+   features {}
+}
+
+variable "resource_group_name" {
+  description = "The name of an existing resource group to be imported."
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = "West Europe"
+}
